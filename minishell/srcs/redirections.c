de4c9871a35c135fcd537c4593bc52aa431c15b0
fsilva24 <fabio@DESKTOP-PQRGQS7>
2025-02-09 15:28:34 +0100

    added redirections

---
+++ minishell/srcs/redirections.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirections.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsilva-p <fsilva-p@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/09 15:26:22 by fsilva-p          #+#    #+#             */
+/*   Updated: 2025/02/09 15:26:22 by fsilva-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+int redirect_input(const char *filename)
+{
+	int fd;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Error Opening File\n");
+		return (-1);
+	}
+	if (dup2(fd, STDIN_FILENO) == -1)
+	{
+		perror("Error redirecting input");
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (0);
+}
+
+int redirect_output(const char *filename)
+{
+	int fd;
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	if (fd == -1)
+	{
+		perror("Error Opening File\n");
+		close(fd);
+		return (-1);
+	}
+	if (dup2(fd, STDIN_FILENO) == -1)
+	{
+		perror("Error redirecting output");
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (0);
+}
