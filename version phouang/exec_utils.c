d51a626b7be30ad529020697ac09f831ce8bb390
Ph-oang <phthu.h@gmail.com>
2025-02-20 13:53:37 +0100

    Update exec_utils.c
    delete free_tab -> utils.c

--- version phouang/exec_utils.c
+++ version phouang/exec_utils.c
@@ -1,20 +1,5 @@
 #include "../minishell.h"
 
-void	free_tab(char **tab)
-{
-    int	i;
-
-    if (!tab)
-        return ;
-    i = 0;
-    while (tab[i])
-    {
-        free(tab[i]);
-        i++;
-    }
-    free(tab);
-}
-
 int	check_cmd_validity(t_cmd *cmd)
 {
 	if (!cmd || !cmd->args || !cmd->args[0])
@@ -29,16 +14,20 @@ int	handle_execve_error(char *cmd_path, char *cmd)
 {
 	int ret;
 
-if (access(cmd_path, X_OK) == -1)
-   {
-       print_cmd_error(cmd, ": Permission denied\n");
-       ret = 126;
-   }
-   else
-   {
-       print_cmd_error(cmd, ": execution failed\n");
-       ret = 127;
-   }
+	ret = 127;
+	if (cmd_path && access(cmd_path, F_OK) == -1)
+	{
+		print_cmd_error(cmd, ": No such file or directory\n");
+	}
+	else if (cmd_path && access(cmd_path, X_OK) == -1)
+	{
+		print_cmd_error(cmd, ": Permission denied\n");
+		ret = 126;
+	}
+	else
+	{
+		print_cmd_error(cmd, ": execution failed\n");
+	}
 	return (ret);
 }
 
@@ -51,5 +40,6 @@ void	print_cmd_error(const char *cmd, const char *message)
     ft_putstr_fd("minishell: ", 2);
     ft_putstr_fd(cmd, 2);
     ft_putstr_fd(message, 2);
-    ft_putstr_fd("\n", 2);
+	if (message[0] && message[ft_strlen(message) - 1] != '\n')
+		ft_putstr_fd("\n", 2);
 }
