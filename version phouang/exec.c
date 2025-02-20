3e1b1170cb686ae6456d092eb68488f34167793f
Ph-oang <phthu.h@gmail.com>
2025-02-20 13:49:12 +0100

    Update exec.c
    add some check & put exc functions in other file (exec2.c)

--- version phouang/exec.c
+++ version phouang/exec.c
@@ -19,7 +19,9 @@ int exec_process(t_cmd *cmd, char **envp)
 {
 	char	*cmd_path;
 	int		status;
-
+	
+	if (!cmd || !envp)
+		return (1);
 	status = check_cmd_validity(cmd);
 	if (status != 0)
 		return (status);
@@ -48,7 +50,7 @@ int exec_cmd(t_cmd *cmd, t_minishell *shell)
    pid = fork();
    if (pid == -1)
    {
-       write(2, "minishell: fork failed\n", 23);
+       print_cmd_error("fork", ": failed\n");
        return (-1);
    }
    if (pid == 0)
@@ -65,67 +67,6 @@ void wait_for_all_processes(void)
 {
 	int		status;
 
-	while (wait(&status) != -1)
+	while (waitpid(-1, &status, 0) > 0)
 		;
-}
-
-static int handle_builtin_part1(t_cmd *cmd, t_minishell *shell)
-{
-    if (ft_strcmp(cmd->args[0], "cd") == 0)
-        return (builtin_cd(cmd, shell));
-    if (ft_strcmp(cmd->args[0], "echo") == 0)
-    {
-        builtin_echo(cmd, &shell->status);
-        return (shell->status);
-    }
-    if (ft_strcmp(cmd->args[0], "exit") == 0)
-    {
-        builtin_exit(cmd, shell, 1);
-        return (shell->status);
-    }
-    if (ft_strcmp(cmd->args[0], "env") == 0)
-    {
-        builtin_env(shell->envp, &shell->status);
-        return (shell->status);
-    }
-    return (-1);
-}
-
-static int handle_builtin_part2(t_cmd *cmd, t_minishell *shell)
-{
-    if (ft_strcmp(cmd->args[0], "pwd") == 0)
-    {
-        builtin_pwd(&shell->status);
-        return (shell->status);
-    }
-    if (ft_strcmp(cmd->args[0], "export") == 0)
-        return (builtin_export(cmd, shell));
-    if (ft_strcmp(cmd->args[0], "unset") == 0)
-        return (builtin_unset(cmd, shell));
-    return (-1);
-}
-
-int handle_builtin(t_cmd *cmd, t_minishell *shell)
-{
-    int ret;
-
-    if (!cmd || !cmd->args[0])
-        return (0);
-    ret = handle_builtin_part1(cmd, shell);
-    if (ret != -1)
-        return (ret);
-    ret = handle_builtin_part2(cmd, shell);
-    if (ret != -1)
-        return (ret);
-    return (0);
-}
-
-void	handle_exit_status(int status, t_minishell *shell)
-{
-  if (!shell)
-        return;
-	if (status >= 0 && status <= 255)
-		shell->exit_status = status;
-	else
-		shell->exit_status = 128 + (status % 256);
 }
