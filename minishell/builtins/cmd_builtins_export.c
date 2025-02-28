5d22e1f8cec1e6d4198d436ba5fab11e4a50995f
Ph-oang <phthu.h@gmail.com>
2025-03-01 00:52:27 +0100

    Update cmd_builtins_export.c

--- minishell/builtins/cmd_builtins_export.c
+++ minishell/builtins/cmd_builtins_export.c
@@ -1,114 +1,131 @@
+
 #include "../include/minishell.h"
 
-char **extract_variable_names(char **envp)
+char	*find_double_var(char *args, char **envp)
 {
-    int     i;
-    char    **variable_names;
+	int		i;
+	char	*name_tmp;
 
-    if (!envp)
-        return (NULL);
-    variable_names = malloc(sizeof(char *) * (size_mass(envp) + 1));
-    if (!variable_names)
-        return (NULL);
-    i = 0;
-    while (envp[i])
-    {
-        if (ft_strchr(envp[i], '='))
-            variable_names[i] = get_variable_name(envp[i]);
-        else
-            variable_names[i] = ft_strdup(envp[i]);
-        if (!variable_names[i])
-        {
-            free_variable_names(variable_names, i);
-            return (NULL);
-        }
-        i++;
-    }
-    variable_names[i] = NULL;
-    return (variable_names);
-}
-
-char **prepare_sorted_env(char **envp)
-{
-    char **tmpmass;
-
-    if (!envp)
-        return (NULL);
-    tmpmass = new_envp(envp);
-    if (!tmpmass)
-    {
-        errmsg("minishell: export: memory allocation error", NULL, NULL, -1);
-        return (NULL);
-    }
-    return (sort_env_vars(tmpmass));
-}
-
-int prepare_export_data(char **tmpmass, char ***before, char ***after)
-{
-    *before = extract_variable_names(tmpmass);
-    *after = remove_quotes_from_env(tmpmass);
-    if (!*before || !*after)
-    {
-        if (*before)
-            free_tab(*before);
-        if (*after)
-            free_tab(*after);
-        return (0);
-    }
-    return (1);
-}
-
-void    print_export_without_args(char **envp)
-{
-    int     i;
-    char    **tmpmass;
-    char    **before;
-    char    **after;
-
-    tmpmass = prepare_sorted_env(envp);
-    if (!tmpmass)
-        return;
-    if (!prepare_export_data(tmpmass, &before, &after))
-    {
-        free_tab(tmpmass);
-        return;
-    }
-    i = -1;
-    while (tmpmass[++i])
-        print_env_entry(before[i], after[i], tmpmass[i]);
-    free_tab(tmpmass);
-    free_tab(before);
-    free_tab(after);
-}
-
-char **builtin_export(char **args, char **envp)
-{
-    int     i;
-    char    *new_value;
-
-    if (!args || !envp)
-        return (envp);
-    if (!args[1])
-    {
-        print_export_without_args(envp);
-        return (envp);
-    }
-    i = 0;
-    while (args[++i])
-    {
-        if (!is_valid_identifier(args[i]))
-        {
-            errmsg("minishell: export: `", args[i], "': not a valid identifier", -1);
-            continue;
-        }
-	if (ft_strchr(args[i], '='))
+	i = 0;
+	name_tmp = malloc(sizeof(char) * (ft_sym_export(args) + 1));
+	if (!name_tmp)
+		return (NULL);
+	while (args[i] != '\0' && args[i] != '=')
 	{
-            new_value = find_env_var(args[i], envp);
-            update_envp(new_value, args[i], &envp);
-            free(new_value);
-        }
-        else
-            update_envp(NULL, args[i], &envp);
-    }
-    return (envp);
+		name_tmp[i] = args[i];
+		i++;
+	}
+	name_tmp[i] = '\0';
+	i = 0;
+	while (envp[i])
+	{
+		if (!ft_strncmp(name_tmp, envp[i], max(ft_strlen(name_tmp),
+					ft_sym_export(envp[i]))))
+			return (name_tmp);
+		i++;
+	}
+	free(name_tmp);
+	return (NULL);
+}
+
+char	**replace_env_var(char **envp, char *args, char *new)
+{
+	char	**new_env;
+	int		j;
+
+	j = 0;
+	new_env = malloc(sizeof(char *) * (size_mass(envp) + 1));
+	if (!new_env)
+		return (NULL);
+	while (envp[j])
+	{
+		if (ft_strncmp(envp[j], new, max(ft_strlen(new),
+					ft_sym_export(envp[j]))))
+			new_env[j] = ft_strdup(envp[j]);
+		else
+			new_env[j] = ft_strdup(args);
+		j++;
+	}
+	new_env[j] = NULL;
+	ft_free(envp);
+	return (new_env);
+}
+
+char	**add_env_var(char **envp, char *str, int free_old)
+{
+	char	**new_env;
+	int		j;
+
+	new_env = malloc(sizeof(char *) * (size_mass(envp) + 2));
+	if (!new_env)
+		return (NULL);
+	j = 0;
+	while (envp[j])
+	{
+		new_env[j] = ft_strdup(envp[j]);
+		j++;
+	}
+	new_env[j] = ft_strdup(str);
+	j++;
+	new_env[j] = NULL;
+	if (free_old)
+		ft_free(envp);
+	return (new_env);
+}
+
+char	**extract_var_values(char **tmpmass)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	**after;
+
+	after = malloc(sizeof(char *) * (size_mass(tmpmass) + 1));
+	if (!after)
+		return (NULL);
+	i = -1;
+	while (tmpmass[++i])
+	{
+		k = 0;
+		after[i] = malloc(sizeof(char) * (ft_strlen(tmpmass[i])
+					- ft_sym_export(tmpmass[i]) + 1));
+		if (!after[i])
+			return (NULL);
+		j = ft_sym_export(tmpmass[i]);
+		while (tmpmass[i][++j] != '\0')
+			after[i][k++] = tmpmass[i][j];
+		after[i][k] = '\0';
+	}
+	after[i] = NULL;
+	return (after);
+}
+
+
+char	**sort_env_vars(char **mass)
+{
+	int		i;
+	int		k;
+	int		j;
+	char	*tmp;
+
+	i = -1;
+	j = size_mass(mass);
+	while (++i < (j - 1))
+	{
+		k = -1;
+		while (++k < (j - i - 1))
+		{
+			if (ft_strncmp(mass[k], mass[k + 1], ft_sym_export(mass[k])) > 0)
+			{
+				tmp = ft_strdup(mass[k]);
+				free(mass[k]);
+				mass[k] = ft_strdup(mass[k + 1]);
+				free(mass[k + 1]);
+				mass[k + 1] = ft_strdup(tmp);
+				free(tmp);
+			}
+		}
+	}
+	mass[i + 1] = NULL;
+	return (mass);
 }
