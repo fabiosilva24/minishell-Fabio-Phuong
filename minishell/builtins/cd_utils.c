98a230db6a455fc5df1104d6fb5051a57ca6b4f3
Ph-oang <phthu.h@gmail.com>
2025-03-01 00:50:08 +0100

    Update cd_utils.c

--- minishell/builtins/cd_utils.c
+++ minishell/builtins/cd_utils.c
@@ -1,126 +1,65 @@
+
 #include "../include/minishell.h"
 
-char *get_cd_env_value(char **envp, char *key)
+void handle_cd_status(int j, int i, char *args, char *str, int *status)
 {
-    char *var;
-    char *value;
-
-    if (!key || !envp)
-        return (NULL);
-    var = find_env_var(key, envp);
-    if (!var)
-        return (NULL);
-    value = ft_strchr(var, '=');
-    if (!value || !*(value + 1))
-    {
-        free(var);
-        return (NULL);
-    }
-    value = ft_strdup(value + 1);
-    free(var);
-    return (value);
+    if (j == 1 && i)
+        chdir(str);
+    else if (j != 1)
+        errmsg("minishell: cd: ", args, ": No such file or directory", -1, status);
+    else if (j == 1 && !i)
+        errmsg("minishell: cd: ", args, ": Not a directory", -1, status);
 }
 
-void update_pwd_variables(char ***envp, char *old_pwd)
+void check_directory_exists(char *path, int j, char *old_path, char *args, int *status)
 {
-    char *current_pwd;
-    char *new_pwd;
-    char *new_oldpwd;
+    DIR             *dir;
+    struct dirent   *entry;
+    struct stat     status_file;
+    int             if_is_cd_cmd;
+    int             i;
 
-    current_pwd = getcwd(NULL, 0);
-    if (!current_pwd)
-        return;
-    new_pwd = ft_strjoin("PWD=", current_pwd);
-    new_oldpwd = ft_strjoin("OLDPWD=", old_pwd);
-    free(current_pwd);
-    if (!new_pwd || !new_oldpwd)
+    if_is_cd_cmd = j;
+    entry = NULL;
+    dir = opendir("./");
+    entry = readdir(dir);
+    j = 0;
+    while (entry != NULL)
     {
-        free(new_pwd);
-        free(new_oldpwd);
-        return;
-    }
-    *envp = replace_env_var(*envp, "PWD", new_pwd);
-    free(new_pwd);
-    if (!*envp)
-    {
-        free(new_oldpwd);
-        return;
-    }
-    *envp = replace_env_var(*envp, "OLDPWD", new_oldpwd);
-    free(new_oldpwd);
-}
-
-void handle_cd_error(char *path)
-{
-    if (path)
-        errmsg("minishell: cd: ", path, ": Permission denied", -1);
-    else
-        errmsg("minishell: cd: ", "error", NULL, -1);
-}
-
-void handle_absolute_path(char *path)
-{
-   if (ft_strlen(path) >= MAX_PATH_LEN)
-	 {
-        errmsg("minishell: cd: ", path, ": Path too long", -1);
-        return;
-    }
-   if (chdir(path) == -1)
-       handle_cd_error(path);
-}
-
-static char *resolve_home_path(char *arg, char **envp)
-{
-    char *home;
-    char *path;
-
-    home = get_cd_env_value(envp, "HOME");
-    if (!home)
-        return (errmsg("minishell: cd: ", NULL, "HOME not set", -1), NULL);
-    if (!arg || *arg == '\0' || !strcmp(arg, "~"))
-        return (home);
-    if (!strncmp(arg, "~/", 2))
-    {
-        if (ft_strlen(home) + ft_strlen(arg) - 1 >= MAX_PATH_LEN)
+        if (ft_strncmp(path, entry->d_name, ft_strlen(entry->d_name)) == 0)
         {
-            free(home);
-            return (errmsg("minishell: cd: ", NULL, "Path too long", -1), NULL);
+            stat(path, &status_file);
+            j = 1;
         }
-        path = ft_strjoin(home, arg + 1);
-        free(home);
-        if (!path)
-            return (errmsg("minishell: cd: ", NULL, "Memory allocation failed", -1), NULL);
-        return (path);
+        entry = readdir(dir);
     }
-    free(home);
-    return (NULL);
+    closedir(dir);
+    i = S_ISDIR(status_file.st_mode);
+    handle_cd_status(j, i, args, path, status);
+    if ((j != 1 || !i) || !if_is_cd_cmd)
+        chdir(old_path);
 }
 
-void handle_relative_path(char **args, char **envp)
+void process_cd_path(char *args, char *old_path, int is_cd_builtin, int *status)
 {
-    char *path;
+    char    **str;
+    int     i;
+    int     j;
 
-    if (!args[1] || !strcmp(args[1], "~") || !strncmp(args[1], "~/", 2))
-        path = resolve_home_path(args[1], envp);
-    else if (!strcmp(args[1], "-"))
-    {
-        path = get_cd_env_value(envp, "OLDPWD");
-        if (!path)
-        {
-            errmsg("minishell: cd: ", NULL, "OLDPWD not set", -1);
-            return;
-        }
-    }
+    j = 0;
+    i = ft_strlen(args);
+    while (args[j] && args[j] != '/')
+        j++;
+    if (j != i)
+        str = ft_split(args, '/');
     else
+        str = ft_split(args, '\0');
+    i = 0;
+    while (str[i])
     {
-        handle_absolute_path(args[1]);
-        return;
+        j = is_cd_builtin;
+        check_directory_exists(str[i], j, old_path, args, status);
+        i++;
     }
-    if (!path)
-        return;
-    if (ft_strlen(path) >= MAX_PATH_LEN)
-        errmsg("minishell: cd: ", NULL, "Path too long", -1);
-    else if (chdir(path) == -1)
-        handle_cd_error(path);
-    free(path);
+    ft_free(str);
 }
