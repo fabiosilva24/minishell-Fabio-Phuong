b39a029dd7c192b9077031698664a792d6c49114
Ph-oang <phthu.h@gmail.com>
2025-03-10 12:44:48 +0100

    Update exit.c
    norm

--- minishell/builtins/exit.c
+++ minishell/builtins/exit.c
@@ -1,10 +1,10 @@
-
 #include "../include/minishell.h"
 
-char    *get_exit_variable_value(char **envp, char *line, int len, int last_exit_status)
+char	*get_exit_variable_value(char **envp, char *line, int len,
+	int last_exit_status)
 {
-    char    *var_name;
-    int     i;
+	char	*var_name;
+	int		i;
 
 	var_name = malloc(sizeof(char) * len);
 	if (!var_name)
@@ -13,8 +13,8 @@ char    *get_exit_variable_value(char **envp, char *line, int len, int last_exit
 	i = -1;
 	while (envp[++i])
 	{
-		if (!ft_strncmp(var_name, envp[i], max(ft_len_eq(envp[i]),
-				ft_strlen(var_name))))
+		if (!ft_strncmp(var_name, envp[i],
+				max(ft_len_eq(envp[i]), ft_strlen(var_name))))
 		{
 			free(var_name);
 			var_name = ft_strdup(envp[i] + ft_len_eq(envp[i]) + 1);
@@ -30,15 +30,17 @@ char    *get_exit_variable_value(char **envp, char *line, int len, int last_exit
 	return (0);
 }
 
-void    replace_exit_status(char **line, int start, int len, char *value, int *i)
+void	replace_exit_status(char **line, int start, int len,
+	char *value, int *i)
 {
 	char	*new_line;
-	int	value_len;
+	int		value_len;
+	char	*old_line;
 
 	value_len = 0;
 	if (value)
 		value_len = ft_strlen(value);
-	new_line = malloc(sizeof(char) * (ft_strlen(*line) + value_len));
+	new_line = malloc(sizeof(char) * (ft_strlen(*line) + value_len + 1));
 	if (!new_line)
 		return ;
 	ft_strlcpy(new_line, *line, start + 1);
@@ -46,15 +48,16 @@ void    replace_exit_status(char **line, int start, int len, char *value, int *i
 		ft_strlcpy(new_line + start, value, ft_strlen(value) + 1);
 	ft_strlcpy(new_line + start + value_len, *line + start + len,
 		ft_strlen(*line + start + len) + 1);
-	free(*line);
-	*line = ft_strdup(new_line);
+	old_line = *line;
+	*line = new_line;
 	(*i) = start;
-	free(new_line);
 	if (value)
 		free(value);
+	free(old_line);
 }
 
-void    extract_and_replace_exit_status(int *i, char **line, char **envp, int last_exit_status)
+void	extract_and_replace_exit_status(int *i, char **line,
+	char **envp, int last_exit_status)
 {
 	int		count;
 	char	*value;
@@ -76,28 +79,30 @@ void    extract_and_replace_exit_status(int *i, char **line, char **envp, int la
 			count++;
 		}
 	}
-	value = get_exit_variable_value(envp, *line + (*i - count), count + 1, last_exit_status);
+	value = get_exit_variable_value(envp, *line + (*i - count),
+			count + 1, last_exit_status);
 	replace_exit_status(line, (*i) - count - 1, count + 1, value, i);
 }
 
-void    expand_exit_status(char **line, char **envp, int last_exit_status)
+void	expand_exit_status(char **line, char **envp, int last_exit_status)
 {
-    int     i;
-    int     single_quote_count;
-    int     double_quote_count;
+	int	i;
+	int	single_quote_count;
+	int	double_quote_count;
 
-    i = 0;
-    single_quote_count = 0;
-    double_quote_count = 0;
-    while ((*line)[i] != '\0')
-    {
-        if ((*line)[i] == '"' && single_quote_count % 2 == 0)
-            double_quote_count++;
-        if ((*line)[i] == '\'')
-            single_quote_count++;
-        if ((single_quote_count % 2 == 0 || double_quote_count % 2 == 1) && (*line)[i] == '$')
-            extract_and_replace_exit_status(&i, line, envp, last_exit_status);
-        else
-            i++;
-    }
+	i = 0;
+	single_quote_count = 0;
+	double_quote_count = 0;
+	while ((*line)[i] != '\0')
+	{
+		if ((*line)[i] == '"' && single_quote_count % 2 == 0)
+			double_quote_count++;
+		if ((*line)[i] == '\'')
+			single_quote_count++;
+		if ((single_quote_count % 2 == 0 || double_quote_count % 2 == 1)
+			&& (*line)[i] == '$')
+			extract_and_replace_exit_status(&i, line, envp, last_exit_status);
+		else
+			i++;
+	}
 }
