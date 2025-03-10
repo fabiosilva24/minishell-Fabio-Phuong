a492b70a2b3a291dfe708fd6b3755e6dee167fa1
Ph-oang <phthu.h@gmail.com>
2025-03-10 12:43:27 +0100

    Update echo.c
    norm

--- minishell/builtins/echo.c
+++ minishell/builtins/echo.c
@@ -12,68 +12,66 @@
 
 #include "../include/minishell.h"
 
-static int is_only_that_char(char *str, char c)
+static int	is_only_that_char(char *str, char c)
 {
-    while (*str)
-    {
-        if (*str != c)
-            return 0;
-        str++;
-    }
-    return 1;
+	while (*str)
+	{
+		if (*str != c)
+			return (0);
+		str++;
+	}
+	return (1);
 }
 
-void execute_echo(t_token *tokens)
+void	execute_echo(t_token *tokens)
 {
-    int arg_count;
-    t_token *current;
-    char **args;
-    int i;
+	int		arg_count;
+	t_token	*current;
+	char	**args;
+	int		i;
 
-    i = 1;
-    arg_count = 0;
-    current = tokens->next;
-    args = malloc(sizeof(char *) * (arg_count + 1));
-    args[arg_count++] = "echo";
-    while (current)
-    {
-        arg_count++;
-        current = current->next;
-    }
-    args[arg_count] = NULL;
-    while (i < arg_count)
-    {
-        parse_input(args[1]);
-        if (i < arg_count - 1)
-            printf(" ");
-        i++;
-    }
-    printf("\n");
-    free(args);
+	i = 1;
+	arg_count = 0;
+	current = tokens->next;
+	args = malloc(sizeof(char *) * (arg_count + 1));
+	args[arg_count++] = "echo";
+	while (current)
+	{
+		arg_count++;
+		current = current->next;
+	}
+	args[arg_count] = NULL;
+	while (i < arg_count)
+	{
+		parse_input(args[1]);
+		if (i < arg_count - 1)
+			printf(" ");
+		i++;
+	}
+	printf("\n");
+	free(args);
 }
 
-void my_echo(int argc, char **argv)
+void	my_echo(int argc, char **argv)
 {
-    int newline; // print a newline default
-    int i;
+	int	newline;
+	int	i;
 
-    newline = 1;
-    i = 1;
-
-    while (argc > 1 && argv[i][0] == '-' && is_only_that_char(argv[i] + 1, 'n'))
-    {
-        newline = 0;
-        i++; //when using echo -n it skips the newline
-    }
-    while (i < argc)
-    {
-        parse_input(argv[i]);
-        if (i < argc - 1)
-            printf(" "); //separate arguments with space
-        i++;
-    }
-    if (newline)
-    {
-        printf("\n");
-    }
+	newline = 1;
+	i = 1;
+	while (argc > 1 && argv[i][0] == '-'
+		&& is_only_that_char(argv[i] + 1, 'n'))
+	{
+		newline = 0;
+		i++;
+	}
+	while (i < argc)
+	{
+		parse_input(argv[i]);
+		if (i < argc - 1)
+			printf(" ");
+		i++;
+	}
+	if (newline)
+		printf("\n");
 }
