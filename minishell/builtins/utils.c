e180f7479201772e4f27dcf9281e1759d4a90b50
Ph-oang <phthu.h@gmail.com>
2025-03-11 17:28:32 +0100

    Update utils.c
    norm

--- minishell/builtins/utils.c
+++ minishell/builtins/utils.c
@@ -1,16 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phoang <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/11 15:53:31 by phoang            #+#    #+#             */
+/*   Updated: 2025/03/11 16:06:36 by phoang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-void errmsg(char *s1, char *s2, char *s3, int code, int *status)
+void	errmsg(char *s1, char *s2, char *s3, int code, int *status)
 {
-    ft_putstr_fd(s1, 2);
-    ft_putstr_fd(s2, 2);
-    ft_putstr_fd(s3, 2);
-    ft_putstr_fd("\n", 2);
-    if (code < 0)
-        *status = -code;
-    else
-        exit(code);
+	ft_putstr_fd(s1, 2);
+	ft_putstr_fd(s2, 2);
+	ft_putstr_fd(s3, 2);
+	ft_putstr_fd("\n", 2);
+	if (code < 0)
+		*status = -code;
+	else
+		exit(code);
 }
 
 int	size_mass(char **envp)
@@ -41,41 +52,25 @@ char	**new_envp(char **envp)
 	return (new_mass);
 }
 
-int ft_sym_export(char *str)
-{
-    int i;
-
-    i = 0;
-    while (str[i] && str[i] != '=')
-        i++;
-    return (i);
-}
-
-void ft_free(char **mass)
-{
-    int sizemass = 0;
-    while (mass[sizemass])
-    {
-        free(mass[sizemass]);
-        sizemass++;
-    }
-    free(mass);
-}
-
-int	ft_len_eq(char *str)
+int	ft_sym_export(char *str)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] != '=' && str[i] != '\0')
+	while (str[i] && str[i] != '=')
 		i++;
 	return (i);
 }
 
-int max(int a, int b)
+void	ft_free(char **mass)
 {
-    if (a > b)
-        return (a);
-    else
-        return (b);
+	int	sizemass;
+
+	sizemass = 0;
+	while (mass[sizemass])
+	{
+		free(mass[sizemass]);
+		sizemass++;
+	}
+	free(mass);
 }
