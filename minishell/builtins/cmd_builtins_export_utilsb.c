dd3016232ba4d7289381ace2d17ae4622b67b85c
Ph-oang <phthu.h@gmail.com>
2025-03-14 02:23:45 +0100

    Update cmd_builtins_export_utilsb.c

--- minishell/builtins/cmd_builtins_export_utilsb.c
+++ minishell/builtins/cmd_builtins_export_utilsb.c
@@ -49,3 +49,50 @@ char	**sort_env_vars(char **mass)
 	mass[i + 1] = NULL;
 	return (mass);
 }
+
+
+char	**add_env_var(char **envp, char *str, int free_old)
+{
+	char	**new_env;
+	int		j;
+
+	j = 0;
+	new_env = malloc(sizeof(char *) * (size_mass(envp) + 2));
+	if (!new_env)
+		return (NULL);
+	if (!copy_existing_vars(new_env, envp, &j))
+		return (NULL);
+	if (!add_new_var(new_env, str, j))
+		return (NULL);
+	new_env[j + 1] = NULL;
+	if (free_old && envp)
+		ft_free(envp);
+	return (new_env);
+}
+
+char	**extract_var_values(char **tmpmass)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	**after;
+
+	after = malloc(sizeof(char *) * (size_mass(tmpmass) + 1));
+	if (!after)
+		return (NULL);
+	i = -1;
+	while (tmpmass[++i])
+	{
+		k = 0;
+		after[i] = malloc(sizeof(char) * (ft_strlen(tmpmass[i])
+					- ft_sym_export(tmpmass[i]) + 1));
+		if (!after[i])
+			return (NULL);
+		j = ft_sym_export(tmpmass[i]);
+		while (tmpmass[i][++j] != '\0')
+			after[i][k++] = tmpmass[i][j];
+		after[i][k] = '\0';
+	}
+	after[i] = NULL;
+	return (after);
+}
