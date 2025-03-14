2b12a551cc0f93cc94c3fb068e578c23c147bf7e
zacka69 <joaorodrigues3099@gmail.com>
2025-03-14 15:44:45 +0100

    norminette

--- minishell/srcs/get_env.c
+++ minishell/srcs/get_env.c
@@ -2,6 +2,18 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   get_env.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joao-alm <joao-alm@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/14 15:23:42 by joao-alm          #+#    #+#             */
+/*   Updated: 2025/03/14 15:40:28 by joao-alm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_env.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+
 	+:+     */
 /*   By: fsilva-p <fsilva-p@student.42.fr>          +#+  +:+
@@ -15,18 +27,34 @@
 
 #include "../include/minishell.h"
 
+static	char	**alloc_env(int count)
+{
+	char	**env;
+
+	env = malloc(sizeof(char *) * (count + 1));
+	if (!env)
+		return (NULL);
+	return (env);
+}
+
+static int	count_environment(void)
+{
+	int	count;
+
+	count = 0;
+	while (environ[count])
+		count++;
+	return (count);
+}
+
 char	**get_environment(void)
 {
 	char	**env;
 	int		i;
 	int		count;
 
-	count = 0;
-	while (environ[count])
-		count++;
-	env = malloc(sizeof(char *) * (count + 1));
-	if (!env)
-		return (NULL);
+	count = count_environment();
+	env = alloc_env(count);
 	i = 0;
 	while (environ[i])
 	{
