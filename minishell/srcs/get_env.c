09f53118762383431d4fc90bcd2c53ecba5d7a01
Ph-oang <phthu.h@gmail.com>
2025-03-14 02:36:53 +0100

    Update get_env.c

--- minishell/srcs/get_env.c
+++ minishell/srcs/get_env.c
@@ -13,12 +13,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/minishell.h"
-
 char	**get_environment(void)
 {
 	char	**env;
+	int		i;
+	int		count;
 
-	env = environ;
+	count = 0;
+	while (environ[count])
+		count++;
+	env = malloc(sizeof(char *) * (count + 1));
+	if (!env)
+		return (NULL);
+	i = 0;
+	while (environ[i])
+	{
+		env[i] = ft_strdup(environ[i]);
+		if (!env[i])
+		{
+			while (i > 0)
+			{
+				free(env[i - 1]);
+				i--;
+			}
+			free(env);
+			return (NULL);
+		}
+		i++;
+	}
+	env[i] = NULL;
 	return (env);
 }
